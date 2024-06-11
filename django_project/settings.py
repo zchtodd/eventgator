62a91377855bd876d7d7c7ad64f4cc694e87d4ca
Zach Todd <zachtodd@pop-os.localdomain>
2024-06-11 07:33:51 -0400

    fix repo parse

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-# st123
+# ssssst123
 # moar testing
 # even more
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
