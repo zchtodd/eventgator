05d8c296489069d61fd2b9e7c1790236986be896
Zach Todd <zachtodd@pop-os.localdomain>
2024-06-12 07:20:55 -0400

    udpates

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-# zmoar testing
-# booooo
-# even more
-# woof
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "widget_tweaks",
     "core",
 ]
 
